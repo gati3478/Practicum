94f1c486713301ebc5faea55329081dba01dd56b
gkink <gkink12@freeuni.edu.ge>
2015-07-07 16:02:04 +0400

    parse.com integration

---
+++ app/src/main/java/ge/edu/freeuni/practicum/view/App.java
@@ -0,0 +1,35 @@
+package ge.edu.freeuni.practicum.view;
+
+import android.app.Application;
+
+import com.parse.FunctionCallback;
+import com.parse.Parse;
+import com.parse.ParseCloud;
+import com.parse.ParseException;
+
+import java.util.HashMap;
+
+/**
+ *
+ * Created by Giorgi on 7/7/2015.
+ */
+public class App extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "mPaILpJR8k0zyaOk5W8Pmywrcs7EE3gYSx2OMajL", "KOctn0qjjJr0TWTN2MZWmcC9pE4va9Wd5tk1OsNK");
+
+        ParseCloud.callFunctionInBackground("hello", new HashMap<String, Object>(), new FunctionCallback<String>() {
+            public void done(String result, ParseException e) {
+                if (e == null) {
+                    System.out.println(result);
+                }
+            }
+        });
+    }
+}
