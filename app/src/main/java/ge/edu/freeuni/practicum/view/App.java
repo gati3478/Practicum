cc86aa8ba98c9219ec006612f215aee58fc53ab9
gkink <gkink12@freeuni.edu.ge>
2015-07-09 12:47:07 +0400

    Created Location class,  App updated

--- app/src/main/java/ge/edu/freeuni/practicum/view/App.java
+++ app/src/main/java/ge/edu/freeuni/practicum/view/App.java
@@ -1,13 +1,7 @@
 package ge.edu.freeuni.practicum.view;
 
 import android.app.Application;
-
-import com.parse.FunctionCallback;
 import com.parse.Parse;
-import com.parse.ParseCloud;
-import com.parse.ParseException;
-
-import java.util.HashMap;
 
 /**
  * Application class.
@@ -20,16 +14,7 @@ public class App extends Application {
 
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
-
         Parse.initialize(this, "mPaILpJR8k0zyaOk5W8Pmywrcs7EE3gYSx2OMajL", "KOctn0qjjJr0TWTN2MZWmcC9pE4va9Wd5tk1OsNK");
-
-        ParseCloud.callFunctionInBackground("hello", new HashMap<String, Object>(), new FunctionCallback<String>() {
-            public void done(String result, ParseException e) {
-                if (e == null) {
-                    System.out.println(result);
-                }
-            }
-        });
     }
 
 }
--- app/src/main/java/ge/edu/freeuni/practicum/view/model/Location.java
+++ app/src/main/java/ge/edu/freeuni/practicum/view/model/Location.java
@@ -5,4 +5,27 @@ package ge.edu.freeuni.practicum.view.model;
  * Created by Giorgi on 7/7/2015.
  */
 public class Location {
+
+    private String name;
+    private int wave;
+
+    public Location(String name, int wave){
+
+    }
+
+    public int getWave() {
+        return wave;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWave(int wave) {
+        this.wave = wave;
+    }
 }
