b8519d98bdd7037e4c006d7cc40e5d6db9a8600c
gati3478 <giopetriashvili@gmail.com>
2015-07-09 19:43:30 +0400

    Finished About page

--- app/src/main/java/ge/edu/freeuni/practicum/view/activity/AboutActivity.java
+++ app/src/main/java/ge/edu/freeuni/practicum/view/activity/AboutActivity.java
@@ -3,18 +3,24 @@ package ge.edu.freeuni.practicum.view.activity;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 import ge.edu.freeuni.practicum.R;
 
 public class AboutActivity extends AppCompatActivity {
 
-    CoordinatorLayout mRootLayout;
+    private String mLogoClickMsg1;
+    private String mLogoClickMsg2;
+    private Random rand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,27 +44,43 @@ public class AboutActivity extends AppCompatActivity {
     }
 
     private void initRootLayout() {
-        mRootLayout = (CoordinatorLayout) findViewById(R.id.root_layout);
+        CoordinatorLayout rootLayout = (CoordinatorLayout) findViewById(R.id.root_layout);
     }
 
     private void initInstances() {
-        FloatingActionButton fabBtn = (FloatingActionButton) findViewById(R.id.fab_btn);
-        fabBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Snackbar.make(mRootLayout, "I'm just pressing buttons", Snackbar.LENGTH_SHORT)
-                        .setAction("Undo", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-
-                            }
-                        })
-                        .show();
-            }
-        });
-
         CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout);
         collapsingToolbarLayout.setTitle(getString(R.string.about_us_title));
+
+        TextView repoText = (TextView) findViewById(R.id.app_github_repo);
+        repoText.setText(Html.fromHtml("GitHub: <a href=\"https://github.com/gati3478/Practicum\">Repository</a>"));
+        repoText.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView gpetr12Email = (TextView) findViewById(R.id.text_view_gpetr12_email);
+        gpetr12Email.setText(Html.fromHtml("<a href=\"mailto:gpetr12@freeuni.edu.ge\">gpetr12@freeuni.edu.ge</a>"));
+        gpetr12Email.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView gpetr12Repo = (TextView) findViewById(R.id.text_view_gpetr12_repo);
+        gpetr12Repo.setText(Html.fromHtml("<a href=\"https://github.com/gati3478\">GitHub</a>"));
+        gpetr12Repo.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView gkink12Email = (TextView) findViewById(R.id.text_view_gkink12_email);
+        gkink12Email.setText(Html.fromHtml("<a href=\"mailto:gkink12@freeuni.edu.ge\">gkink12@freeuni.edu.ge</a>"));
+        gkink12Email.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView gkink12Repo = (TextView) findViewById(R.id.text_view_gkink12_repo);
+        gkink12Repo.setText(Html.fromHtml("<a href=\"https://github.com/gkink\">GitHub</a>"));
+        gkink12Repo.setMovementMethod(LinkMovementMethod.getInstance());
+
+        mLogoClickMsg1 = getString(R.string.logo_click_msg_1);
+        mLogoClickMsg2 = getString(R.string.logo_click_msg_2);
+
+        rand = new Random();
+    }
+
+    public void onLogoClick(View view) {
+        int res = rand.nextInt(2);
+        String msg = res == 0 ? mLogoClickMsg1 : mLogoClickMsg2;
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
 }
